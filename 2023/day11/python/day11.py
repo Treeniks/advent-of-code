9fc8653b52835bd4228bd8c4a6916beaaab2c050
Thomas Lindae <thomas.lindae@in.tum.de>
2023-12-11 08:32:43 +0100

    solved 2023 day11 part 2 in python

--- 2023/day11/python/day11.py
+++ 2023/day11/python/day11.py
@@ -25,55 +25,55 @@ def get_column(input, column):
         result.append(input[i][column])
     return result
 
-def insert_column(input, column, insert):
-    for i in range(0, len(input)):
-        input[i] = input[i][:column] + insert[i] + input[i][column:]
-
-def expand(input):
-    insert_r = "." * len(input[0])
-    to_insert = []
-    for row in range(0, len(input)):
-        if no_galaxies(input[row]):
-            to_insert.append(row)
-
-    counter = 0
-    for row in to_insert:
-        input.insert(row + counter, insert_r)
-        counter += 1
-
-    insert_c = "." * len(input)
-    to_insert = []
-    for column in range(0, len(input[0])):
-        if no_galaxies(get_column(input, column)):
-            to_insert.append(column)
-
-    counter = 0
-    for column in to_insert:
-        insert_column(input, column + counter, insert_c)
-        counter += 1
-
 def get_galaxies(input):
     result = []
     for row in range(0, len(input)):
         for column in range(0, len(input[0])):
             if input[row][column] == '#':
-                result.append((row, column))
+                result.append((column, row))
     return result
 
-def part1(input):
-    expand(input)
+def number_of_empty_rows_between(input, begin, end):
+    if begin > end:
+        tmp = begin
+        begin = end
+        end = tmp
+    result = 0
+    for row in range(begin, end):
+        if no_galaxies(input[row]):
+            result += 1
+    return result
+
+def number_of_empty_columns_between(input, begin, end):
+    if begin > end:
+        tmp = begin
+        begin = end
+        end = tmp
+    result = 0
+    for column in range(begin, end):
+        if no_galaxies(get_column(input, column)):
+            result += 1
+    return result
+
+def solve(input, multiplier):
     galaxies = get_galaxies(input)
 
     distances = []
 
     for ((x1, y1), (x2, y2)) in itertools.combinations(galaxies, 2):
-        distance = abs(x2 - x1) + abs(y2 - y1)
+        num1 = number_of_empty_rows_between(input, y1, y2)
+        num2 = number_of_empty_columns_between(input, x1, x2)
+        distance = abs(x2 - x1) + num2 * (multiplier - 1) + abs(y2 - y1) + num1 * (multiplier - 1)
         distances.append(distance)
 
     return sum(distances)
 
 if __name__ == "__main__":
     input = list(sys.stdin.read().strip().splitlines())
+    # input = list(example.strip().splitlines())
 
-    result = part1(input)
-    print(f"Part 1: {result}")
+    result_part1 = solve(input, 2)
+    print(f"Part 1: {result_part1}")
+
+    result_part2 = solve(input, 1000000)
+    print(f"Part 2: {result_part2}")
