1886684ff83543b86e8a9a6b0398c85c1163ae5a
Thomas Lindae <thomas@lindae.de>
2023-12-12 17:22:44 +0100

    2023 day12 rust optimizations, but still too slow for part 2

--- 2023/day12/rust/src/main.rs
+++ 2023/day12/rust/src/main.rs
@@ -38,7 +38,7 @@ fn parse_line(line: &str) -> Line {
     Line { springs, records }
 }
 
-fn is_correct(springs: &[Spring], records: &[usize]) -> bool {
+fn check(springs: &[Spring], records: &[usize]) -> bool {
     let mut groups: Vec<usize> = vec![];
 
     let mut b = false;
@@ -59,7 +59,28 @@ fn is_correct(springs: &[Spring], records: &[usize]) -> bool {
     groups == records
 }
 
-fn combinations(springs: &[Spring], records: &[usize]) -> usize {
+fn check_until(springs: &[Spring], records: &[usize], until: usize) -> bool {
+    let mut groups: Vec<usize> = vec![];
+
+    let mut b = false;
+    for s in springs.iter().take(until) {
+        match (s, b) {
+            (Spring::Broken, true) => {
+                let i = groups.len() - 1;
+                groups[i] += 1;
+            }
+            (Spring::Broken, false) => {
+                groups.push(1);
+                b = true;
+            }
+            _ => b = false,
+        }
+    }
+
+    groups.len() <= records.len() && groups == records[0..groups.len()]
+}
+
+fn combinations(springs: &mut [Spring], records: &[usize]) -> usize {
     let first_unknown = match springs
         .iter()
         .enumerate()
@@ -67,7 +88,7 @@ fn combinations(springs: &[Spring], records: &[usize]) -> usize {
     {
         Some((i, _)) => i,
         None => {
-            if is_correct(springs, records) {
+            if check(springs, records) {
                 return 1;
             } else {
                 return 0;
@@ -75,23 +96,35 @@ fn combinations(springs: &[Spring], records: &[usize]) -> usize {
         }
     };
 
-    let mut tmp = Vec::with_capacity(springs.len());
-    tmp.extend_from_slice(springs);
+    if let Some((i, _)) = springs[..first_unknown]
+        .iter()
+        .enumerate()
+        .filter(|&(_, &s)| s == Spring::Operational)
+        .last()
+    {
+        if !check_until(springs, records, i) {
+            return 0;
+        }
+    };
 
-    let mut tmp2 = Vec::with_capacity(springs.len());
-    tmp2.extend_from_slice(springs);
+    springs[first_unknown] = Spring::Broken;
+    let r1 = combinations(springs, records);
+    springs[first_unknown] = Spring::Operational;
+    let r2 = combinations(springs, records);
+    springs[first_unknown] = Spring::Unknown;
 
-    tmp[first_unknown] = Spring::Broken;
-    tmp2[first_unknown] = Spring::Operational;
-
-    return combinations(&tmp, records) + combinations(&tmp2, records);
+    return r1 + r2;
 }
 
 fn extend_part2(springs: &mut Vec<Spring>, records: &mut Vec<usize>) {
     let clone = springs.clone();
+    springs.push(Spring::Unknown);
     springs.extend(clone.clone());
+    springs.push(Spring::Unknown);
     springs.extend(clone.clone());
+    springs.push(Spring::Unknown);
     springs.extend(clone.clone());
+    springs.push(Spring::Unknown);
     springs.extend(clone);
 
     let clone = records.clone();
@@ -102,9 +135,9 @@ fn extend_part2(springs: &mut Vec<Spring>, records: &mut Vec<usize>) {
 }
 
 fn part1(input: &str) -> usize {
-    let lines: Vec<Line> = input.trim().lines().map(parse_line).collect();
-    return lines.iter().fold(0, |acc, line| {
-        acc + combinations(&line.springs, &line.records)
+    let mut lines: Vec<Line> = input.trim().lines().map(parse_line).collect();
+    return lines.iter_mut().fold(0, |acc, line| {
+        acc + combinations(&mut line.springs, &line.records)
     });
 }
 
@@ -113,17 +146,18 @@ fn part2(input: &str) -> usize {
     lines
         .iter_mut()
         .for_each(|l| extend_part2(&mut l.springs, &mut l.records));
-    return lines.iter().fold(0, |acc, line| {
-        acc + combinations(&line.springs, &line.records)
+    return lines.iter_mut().fold(0, |acc, line| {
+        acc + combinations(&mut line.springs, &line.records)
     });
 }
 
 fn main() -> Result<(), std::io::Error> {
     let mut input = String::new();
     let _ = std::io::stdin().read_to_string(&mut input)?;
+    // let input = "?###???????? 3,2,1";
 
     println!("Part 1: {}", part1(&input));
-    // println!("Part 2: {}", part2(&input));
+    println!("Part 2: {}", part2(&input));
 
     Ok(())
 }
@@ -148,19 +182,19 @@ mod tests {
         assert_eq!(expected, result);
     }
 
-    //     #[test]
-    //     fn test_part2() {
-    //         let input = "???.### 1,1,3
-    // .??..??...?##. 1,1,3
-    // ?#?#?#?#?#?#?#? 1,3,1,6
-    // ????.#...#... 4,1,1
-    // ????.######..#####. 1,6,5
-    // ?###???????? 3,2,1
-    // ";
-    //
-    //         let expected = 525152;
-    //         let result = part2(input);
-    //
-    //         assert_eq!(expected, result);
-    //     }
+    #[test]
+    fn test_part2() {
+        let input = "???.### 1,1,3
+    .??..??...?##. 1,1,3
+    ?#?#?#?#?#?#?#? 1,3,1,6
+    ????.#...#... 4,1,1
+    ????.######..#####. 1,6,5
+    ?###???????? 3,2,1
+    ";
+
+        let expected = 525152;
+        let result = part2(input);
+
+        assert_eq!(expected, result);
+    }
 }
