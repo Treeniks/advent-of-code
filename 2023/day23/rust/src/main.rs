382e8ba9fc2b83a9a8b447bc9bb783a95a7f33d2
Thomas Lindae <thomas.lindae@in.tum.de>
2023-12-23 06:42:41 +0100

    2023 day23 rust: solved part 2

--- 2023/day23/rust/src/main.rs
+++ 2023/day23/rust/src/main.rs
@@ -151,7 +151,7 @@ impl IndexMut<usize> for Grid {
     }
 }
 
-fn walk_path(grid: Grid, (x, y): (usize, usize), path: usize, paths: &mut Vec<usize>) {
+fn walk_path_part1(grid: Grid, (x, y): (usize, usize), path: usize, paths: &mut Vec<usize>) {
     if y == grid.rows - 1 {
         paths.push(path);
     }
@@ -161,21 +161,22 @@ fn walk_path(grid: Grid, (x, y): (usize, usize), path: usize, paths: &mut Vec<us
         Tile::Path | Tile::Slope(Direction::Up) => {
             let mut clone = grid.clone();
             clone[y - 1][x] = Tile::Walked;
-            walk_path(clone, (x, y - 1), path + 1, paths);
+            walk_path_part1(clone, (x, y - 1), path + 1, paths);
         }
         _ => {}
     }
 
     // check down
+    // I have no idea why I need this guard
     if y + 1 < grid.rows {
-    match grid[y + 1][x] {
-        Tile::Path | Tile::Slope(Direction::Down) => {
-            let mut clone = grid.clone();
-            clone[y + 1][x] = Tile::Walked;
-            walk_path(clone, (x, y + 1), path + 1, paths);
+        match grid[y + 1][x] {
+            Tile::Path | Tile::Slope(Direction::Down) => {
+                let mut clone = grid.clone();
+                clone[y + 1][x] = Tile::Walked;
+                walk_path_part1(clone, (x, y + 1), path + 1, paths);
+            }
+            _ => {}
         }
-        _ => {}
-    }
     }
 
     // check left
@@ -183,7 +184,7 @@ fn walk_path(grid: Grid, (x, y): (usize, usize), path: usize, paths: &mut Vec<us
         Tile::Path | Tile::Slope(Direction::Left) => {
             let mut clone = grid.clone();
             clone[y][x - 1] = Tile::Walked;
-            walk_path(clone, (x - 1, y), path + 1, paths);
+            walk_path_part1(clone, (x - 1, y), path + 1, paths);
         }
         _ => {}
     }
@@ -193,7 +194,60 @@ fn walk_path(grid: Grid, (x, y): (usize, usize), path: usize, paths: &mut Vec<us
         Tile::Path | Tile::Slope(Direction::Right) => {
             let mut clone = grid.clone();
             clone[y][x + 1] = Tile::Walked;
-            walk_path(clone, (x + 1, y), path + 1, paths);
+            walk_path_part1(clone, (x + 1, y), path + 1, paths);
+        }
+        _ => {}
+    }
+}
+
+fn walk_path_part2(grid: &mut Grid, (x, y): (usize, usize), path: usize, paths: &mut Vec<usize>) {
+    if y == grid.rows - 1 {
+        paths.push(path);
+    }
+
+    // check up
+    match grid[y - 1][x] {
+        Tile::Path | Tile::Slope(_) => {
+            let tile_before = grid[y - 1][x];
+            grid[y - 1][x] = Tile::Walked;
+            walk_path_part2(grid, (x, y - 1), path + 1, paths);
+            grid[y - 1][x] = tile_before;
+        }
+        _ => {}
+    }
+
+    // check down
+    // I have no idea why I need this guard
+    if y + 1 < grid.rows {
+        match grid[y + 1][x] {
+            Tile::Path | Tile::Slope(_) => {
+                let tile_before = grid[y + 1][x];
+                grid[y + 1][x] = Tile::Walked;
+                walk_path_part2(grid, (x, y + 1), path + 1, paths);
+                grid[y + 1][x] = tile_before;
+            }
+            _ => {}
+        }
+    }
+
+    // check left
+    match grid[y][x - 1] {
+        Tile::Path | Tile::Slope(_) => {
+            let tile_before = grid[y][x - 1];
+            grid[y][x - 1] = Tile::Walked;
+            walk_path_part2(grid, (x - 1, y), path + 1, paths);
+            grid[y][x - 1] = tile_before;
+        }
+        _ => {}
+    }
+
+    // check right
+    match grid[y][x + 1] {
+        Tile::Path | Tile::Slope(_) => {
+            let tile_before = grid[y][x + 1];
+            grid[y][x + 1] = Tile::Walked;
+            walk_path_part2(grid, (x + 1, y), path + 1, paths);
+            grid[y][x + 1] = tile_before;
         }
         _ => {}
     }
@@ -214,15 +268,29 @@ fn part1(input: &str) -> usize {
     let (x, y) = (start_index, 1usize);
 
     let mut result = Vec::new();
-    walk_path(grid, (x, y), 1, &mut result);
+    walk_path_part1(grid, (x, y), 1, &mut result);
 
     *result.iter().max().unwrap()
 }
 
 fn part2(input: &str) -> usize {
-    let grid = Grid::from_input(input);
+    let mut grid = Grid::from_input(input);
 
-    0
+    let start_index = grid[0]
+        .iter()
+        .enumerate()
+        .find(|&(_, &t)| t == Tile::Path)
+        .unwrap()
+        .0;
+
+    grid[0][start_index] = Tile::Walked;
+
+    let (x, y) = (start_index, 1usize);
+
+    let mut result = Vec::new();
+    walk_path_part2(&mut grid, (x, y), 1, &mut result);
+
+    *result.iter().max().unwrap()
 }
 
 fn main() -> Result<(), std::io::Error> {
